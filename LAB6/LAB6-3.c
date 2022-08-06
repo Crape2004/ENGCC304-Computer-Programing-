bc4e91185cbec0f7f87091974f4437939a328e67
Crape2004 <108038830+Crape2004@users.noreply.github.com>
2022-08-07 00:48:30 +0700

    ใบงาน

---
+++ LAB6/LAB6-3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+        int a ;
+        printf("Enter Your Line : ") ;
+        scanf("%d", &a) ;
+        if(a % 2 == 0) {
+            for(int y = 1 ; y <= a ; y++) {
+                for(int x = a-1 ; x >= y ; x --) {
+                    printf(" ") ;
+            }//end for
+                for(int x = 1 ; x <= y ; x++) {
+                    printf("* ") ;
+                }//end for
+            printf("\n") ;
+            }//end for
+        } else
+            for(int y = 1 ; y <= a ; y++) {
+                for(int x = 1 ; x <= y ; x++) {
+                    printf(" ") ;
+                }//end for
+            for(int x = a ; x >= y ; x--) {
+                printf("* ") ;
+                }//end for
+            printf("\n") ;
+            }//end for
+    return 0;
+}//end function 
